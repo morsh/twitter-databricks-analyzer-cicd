25b6d558e77989e62b9426462ee70d049e8377b9
morsh <mor.shemesh@gmail.com>
2018-08-01 17:41:38 +0300

    update login

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,3 +4,4 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
+make requirements
