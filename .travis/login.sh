8dfd904cad73aa1cfda961aa9cbd5c427f952c62
morsh <mor.shemesh@gmail.com>
2018-08-01 17:28:31 +0300

    trwip 8

--- .travis/login.sh
+++ .travis/login.sh
@@ -1,4 +1,2 @@
 az login --service-principal --username $SERVICE_PRINCIPAL_APP_ID --password $SERVICE_PRINCIPAL_PASSWORD --tenant $SERVICE_PRINCIPAL_TENANT_ID
 az account list
-
-echo "az login successfull"
