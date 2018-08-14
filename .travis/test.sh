7b2b0c7548594e234be7e8c0521cb771cb6759cc
morsh <mor.shemesh@gmail.com>
2018-08-14 17:10:27 +0300

    update test

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,11 +1,15 @@
 make configure_databricks & # send the long living command to background!
 
+# Constants
+RED='\033[0;31m'
 minutes=0
 limit=3
+
 while kill -0 $! >/dev/null 2>&1; do
   echo -n -e " \b" # never leave evidences!
 
   if [ $minutes == $limit ]; then
+    echo "${RED}Test has reached the ${minutes} timeout limit"
     exit 1
   fi
 
