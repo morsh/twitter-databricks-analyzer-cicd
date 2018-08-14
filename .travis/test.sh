5e5ee52590016ae94f8ddcfdf1949ef8bfce90fa
morsh <mor.shemesh@gmail.com>
2018-08-14 16:12:39 +0300

    trying travis differently

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,2 +1,17 @@
-make configure_databricks
+make configure_databricks & # send the long living command to background!
+
+minutes=0
+limit=3
+while kill -0 $! >/dev/null 2>&1; do
+  echo -n -e " \b" # never leave evidences!
+
+  if [ $minutes == $limit ]; then
+    break;
+  fi
+
+  minutes=$((minutes+1))
+
+  sleep 60
+done
+
 exit 0
